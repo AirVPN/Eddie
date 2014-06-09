c455fadb258bfeb2bc34e5c5e3620994417a544e
Clodo76 <clodo@clodo.it>
2014-06-09 16:11:59 +0200

    osx

---
+++ src/Core/Process.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirVPN.Core
+{
+	public class Process : System.Diagnostics.Process
+	{	
+		public bool ReallyExited
+		{
+			get
+			{
+				bool check1 = HasExited;
+
+				bool check2 = true;
+				System.Diagnostics.Process[] processlist = Process.GetProcesses();
+				foreach (System.Diagnostics.Process theprocess in processlist)
+				{
+					try
+					{
+						if (theprocess.Id == this.Id)
+							check2 = false;
+					}
+					catch (System.InvalidOperationException)
+					{
+					}
+				}
+
+				if ((check1 == true) && (check2 == false))
+				{
+					Console.WriteLine("Unexpected: process marked as exited but still running.");
+				}
+
+				return check2;
+			}
+		}
+	}
+}
