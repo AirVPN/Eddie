3d14d208ecb2d4eedf06aaf25f752e276605d902
Clodo <clodo@clodo.it>
2016-10-07 22:37:51 +0200

    2.11.5

--- src/UI.Cocoa.Osx/Main.cs
+++ src/UI.Cocoa.Osx/Main.cs
@@ -36,7 +36,6 @@ namespace Eddie.UI.Osx
 
 			// Due to a bug in Xamarin, that don't recognize resources inside Core library if Mono is bundled, we embed some resources in entry assembly.
 
-			//Core.ResourcesFiles.LoadString (Assembly.GetEntryAssembly (), "AirVPN.xml", "AirVPN.xml");
 			Core.ResourcesFiles.LoadString (Assembly.GetEntryAssembly (), "license.txt", "License.txt");
 			Core.ResourcesFiles.LoadString (Assembly.GetEntryAssembly (), "thirdparty.txt", "ThirdParty.txt");
 			Core.ResourcesFiles.LoadString (Assembly.GetEntryAssembly (), "tos.txt", "TOS.txt");
--- src/UI.Cocoa.Osx/postbuild.sh
+++ src/UI.Cocoa.Osx/postbuild.sh
@@ -1,3 +1,20 @@
+mkdir -p bin/Debug/AirVPN.app/Contents/MacOS/Providers/
+mkdir -p bin/Release/AirVPN.app/Contents/MacOS/Providers/
+
+cp ../../Providers/*.xml bin/Debug/AirVPN.app/Contents/MacOS/Providers/
+cp ../../Providers/*.xml bin/Release/AirVPN.app/Contents/MacOS/Providers/
+
+sed "s/{@version}/$(cat ../../version.txt)/" ../../resources/osx/Info.plist > bin/Debug/AirVPN.app/Contents/Info.plist
+sed "s/{@version}/$(cat ../../version.txt)/" ../../resources/osx/Info.plist > bin/Release/AirVPN.app/Contents/Info.plist
+
+chmod 755 ../../deploy/osx_x64/openvpn
+cp ../../deploy/osx_x64/openvpn bin/Debug/AirVPN.app/Contents/MacOS/
+cp ../../deploy/osx_x64/openvpn bin/Release/AirVPN.app/Contents/MacOS/
+
+chmod 755 ../../deploy/osx_x64/stunnel
+cp ../../deploy/osx_x64/stunnel bin/Debug/AirVPN.app/Contents/MacOS/
+cp ../../deploy/osx_x64/stunnel bin/Release/AirVPN.app/Contents/MacOS/
+
 codesign -v --force --sign "Developer ID Application: Fabrizio Carimati (SQ9X79YUY3)" "/Users/clodo/Documents/airvpn-client/src/UI.Cocoa.Osx/bin/Release/AirVPN.app/Contents/MonoBundle/libMonoPosixHelper.dylib"
 codesign -v --force --sign "Developer ID Application: Fabrizio Carimati (SQ9X79YUY3)" "/Users/clodo/Documents/airvpn-client/src/UI.Cocoa.Osx/bin/Release/AirVPN.app/Contents/MacOS/openvpn"
 codesign -v --force --sign "Developer ID Application: Fabrizio Carimati (SQ9X79YUY3)" "/Users/clodo/Documents/airvpn-client/src/UI.Cocoa.Osx/bin/Release/AirVPN.app/Contents/MacOS/stunnel"
