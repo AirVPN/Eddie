23954a1cb39bb0e537ef698a148756187dba9ce0
Clodo76 <clodo@clodo.it>
2017-07-22 00:29:29 +0200

    2.13.3

---
+++ src/Platforms.Windows/Native.cs
@@ -0,0 +1,82 @@
+﻿// <eddie_source_header>
+// This file is part of Eddie/AirVPN software.
+// Copyright (C)2014-2016 AirVPN (support@airvpn.org) / https://airvpn.org
+//
+// Eddie is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Eddie is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Eddie. If not, see <http://www.gnu.org/licenses/>.
+// </eddie_source_header>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Xml;
+using Eddie.Core;
+using Microsoft.Win32;
+
+namespace Eddie.Platforms.Windows
+{
+	public static class Native
+	{
+		private static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+
+		private const uint FILE_READ_EA = 0x0008;
+		private const uint FILE_FLAG_BACKUP_SEMANTICS = 0x2000000;
+
+		[DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		static extern uint GetFinalPathNameByHandle(IntPtr hFile, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszFilePath, uint cchFilePath, uint dwFlags);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool CloseHandle(IntPtr hObject);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern IntPtr CreateFile(
+				[MarshalAs(UnmanagedType.LPTStr)] string filename,
+				[MarshalAs(UnmanagedType.U4)] uint access,
+				[MarshalAs(UnmanagedType.U4)] FileShare share,
+				IntPtr securityAttributes, // optional SECURITY_ATTRIBUTES struct or IntPtr.Zero
+				[MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+				[MarshalAs(UnmanagedType.U4)] uint flagsAndAttributes,
+				IntPtr templateFile);
+
+		public static string GetFinalPathName(string path)
+		{
+			var h = CreateFile(path,
+				FILE_READ_EA,
+				FileShare.ReadWrite | FileShare.Delete,
+				IntPtr.Zero,
+				FileMode.Open,
+				FILE_FLAG_BACKUP_SEMANTICS,
+				IntPtr.Zero);
+			if (h == INVALID_HANDLE_VALUE)
+				return "";
+
+			try
+			{
+				var sb = new StringBuilder(1024);
+				var res = GetFinalPathNameByHandle(h, sb, 1024, 0);
+				if (res == 0)
+					return "";
+
+				return sb.ToString();
+			}
+			finally
+			{
+				CloseHandle(h);
+			}
+		}
+	}
+}
